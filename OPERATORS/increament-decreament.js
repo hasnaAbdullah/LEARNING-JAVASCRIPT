26daaa3abcf95844f8290e7901407d5b621e0009
greenFruits <mdabdullah6548ctg@gmail.com>
2025-02-07 22:34:42 +0600

    I have understood the operation

---
+++ OPERATORS/increament-decreament.js
@@ -0,0 +1,6 @@
+let x = 3;
+let a = 5;
+let y = x++; //at first x is assigned. as a result y = 3. and then update x
+let z = ++a; //at first update and then assign a in z
+// let z = ++x;
+console.log(z);
