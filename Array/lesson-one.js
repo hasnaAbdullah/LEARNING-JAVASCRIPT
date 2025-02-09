807d8a5414f5020bc40459accf4bbd8793cb3c64
greenFruits <mdabdullah6548ctg@gmail.com>
2025-02-09 12:16:01 +0600

    I have learned all important methods of array

--- Array/lesson-one.js
+++ Array/lesson-one.js
@@ -1,1 +1,17 @@
-let fruits = ["Banana", "Orange", "Apple"];
+let fruits = ["Banana", "Orange", "Orange", "Mango", "Orange", "Apple"];
+// let returnValue = fruits.push("mango");
+// let returnValue = fruits.pop();
+// let returnValue = fruits.unshift("mango");
+// let returnValue = fruits.shift();
+// let returnValue = fruits.sort();
+// let returnValue = fruits.sort().reverse();
+// let returnValue = fruits.at(4);
+// let returnValue = fruits.includes("Orange");
+// let returnValue = fruits.indexOf("Orange");
+// let returnValue = fruits.lastIndexOf("Orange");
+// let returnValue = fruits.slice(1, 4 + 1);
+// let returnValue = fruits.splice(2, 0, "Watermelon", "Jackfruit");
+// let returnValue = fruits.join(" . ");
+
+// console.log(fruits);
+console.log(returnValue);
